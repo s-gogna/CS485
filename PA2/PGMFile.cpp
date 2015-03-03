209815a5ebd4269c78faac85e18b94f3c6abec33
Shubham K Gogna <s-gogna@users.noreply.github.com>
2015-03-02 21:55:27 -0800

    Started PA2

---
+++ PA2/PGMFile.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+typedef unsigned char byte;
+
+class PGMFile
+{
+	public:
+		PGMFile();
+		PGMFile( int, int );
+		~PGMFile();
+		PGMFile( const PGMFile& );
+
+		void read( const char* filename );
+		void write( const char* filename );
+
+		byte& at( int, int );
+
+	private:
+		byte** data;
+		int width;
+		int height;
+};
+
+PGMFile::PGMFile()
+{
+	data = NULL;
+	width = height = 0;
+}
+
+PGMFile::PGMFile( int h, int w )
+{
+	height = h;
+	width = w;
+	data = new byte*[height];
+	for( int i = 0; i < height; ++i )
+	{
+		data[i] = new byte[width];
+	}
+}
+
+PGMFile::~PGMFile()
+{
+	// Deallocate memory
+	for( int i = 0; i < height; ++i )
+	{
+		delete[] data[i];
+	}
+	delete[] data;
+	data = NULL;
+
+	// Reset values to 0
+	width = height = 0;
+}
+
+PGMFile::PGMFile( const PGMFile& src )
+{
+	// Copy data
+	width = src.width;
+	height = src.height;
+
+	// Allocate and copy
+	data = new byte*[ height ];
+	for( int i = 0; i < height; ++i )
+	{
+		// Allocate
+		data[i] = new byte[width];
+
+		// Copy
+		for( int j = 0; j < width; ++j )
+		{
+			data[i][j] = src.data[i][j];
+		}
+	}
+}
+
+void PGMFile::read( const char* filename )
+{
+	// Open the file
+	char buf[50];
+	ifstream infile( filename );
+
+	// Read the first word
+	infile >> buf;
+
+	// Read the width and then height and then the P5 max value
+	infile >> width >> height >> buf;
+
+	// Read the newline
+	infile.get();
+
+	// Allocate memory
+	unsigned char temp;
+	data = new byte*[ height ];
+	for( int i = 0; i < height; ++i )
+	{
+		// Allocate
+		data[i] = new byte[width];
+
+		// Read
+		for( int j = 0; j < width; ++j )
+		{
+			data[i][j] = infile.get();
+		}
+	}
+
+	// Close the file
+	infile.close();
+}
+
+void PGMFile::write( const char* filename )
+{
+	// Open the file
+	ofstream outfile( filename );
+
+	// Output the basic information
+	outfile << "P5" << endl << width << ' ' << height << endl << "255" << endl;
+
+	// Loop through the data and output
+	for( int i = 0; i < height; ++i )
+	{
+		for( int j = 0; j < width; ++j )
+		{
+			outfile << data[i][j];
+		}
+	}
+
+	// Close the file
+	outfile.close();
+}
+
+byte& PGMFile::at( int i, int j )
+{
+	// Check bounds
+	if( i >= 0 && i < height && j >= 0 && j < width )
+	{
+		return data[i][j];
+	}	
+	else
+	{
+		throw "Invalid index arguments for at(i,j)";
+	}
+}
+
+
+
+
+
+
---
+++ PA2/Program.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include "PGMFile.cpp"
+#include "solve_system.c"
+#include "svbksb.c"
+#include "svdcmp.c"
+using namespace std;
+
+int main( int argc, char** argv )
+{
+	// Initialize variables
+	// Note: solver requires indexing of 1, therefore extra space is
+	// allocated and unused
+	PGMFile source;
+	PGMFile dest(48, 40); // (height, width)
+	float** A;
+	float coef_x[4];
+	float B_x[4];
+	float coef_y[4];
+	float B_y[4];
+
+	// Set the B vectors
+	B_x[1] = 8;
+	B_x[2] = 31;
+	B_x[3] = 20;
+	B_x[4] = 20;
+
+	B_y[1] = 10;
+	B_y[2] = 10;
+	B_y[3] = 27;
+	B_y[4] = 38;
+
+	// Allocate A and set values
+	A = new float*[5];
+	for( int i = 0; i < 5; ++i )
+	{
+		A[i] = new float[4];
+	}
+
+	// Check arguments
+	if( argc < 10 )
+	{
+		// Return error
+		cout << "Invalid usage: ./a.out filename eleft_x eleft_y eright_x eright_y nose_x nose_y mouth_x mouth_y" << endl;
+		return -1;
+	}
+
+	// Read the image and set the coordinates
+	source.read( argv[1] );
+	
+	A[1][1] = atof( argv[2] );
+	A[1][2] = atof( argv[3] );
+	A[1][3] = 1.0;
+
+	A[2][1] = atof( argv[4] );
+	A[2][2] = atof( argv[5] );
+	A[2][3] = 1.0;
+
+	A[3][1] = atof( argv[6] );
+	A[3][2] = atof( argv[7] );
+	A[3][3] = 1.0;
+	
+	A[4][1] = atof( argv[8] );
+	A[4][2] = atof( argv[9] );
+	A[4][3] = 1.0;
+
+cout << "Matrix A" << endl;
+for( int i = 1; i<= 4; ++i ) { for( int j = 1; j <= 3; ++j ) cout << A[i][j] << ' '; cout << endl; }
+
+	// Solve
+	solve_system( 4, 3, A, coef_x, B_x );
+	solve_system( 4, 3, A, coef_y, B_y );
+
+cout << "Vector X, Vector Y" << endl;
+for( int i = 1; i < 4; ++i ) cout << coef_x[i] << ',' << coef_y[i] << endl;
+
+	cout << img.at(0,0) << endl;
+
+	// Start writing to the destination image
+	for( int i = 0; i < 112; ++i )
+	{
+		for( int j = 0; j < 92; ++j )
+		{
+			// Get the remapped value
+			int final_x = j * coef_x[1] + source
+			int final_y = 
+		}
+	}
+
+	// Return success
+	return 0;
+}
